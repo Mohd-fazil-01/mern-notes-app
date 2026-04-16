f4f9c95684b4eee85833f8e8f25d3fe8fe0d0643
Mohd Fazil <fazilansari038@gmail.com>
2026-04-17 03:23:36 +0530

    Update api.js
    change the base url of backend in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mern-notes-app-7m51.onrender.com',
   withCredentials: true, 
 });
 
