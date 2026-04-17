65005e625c5992c3f1ea10ccd70a4ca27e5b014b
Mohd Fazil <fazilansari038@gmail.com>
2026-04-17 10:35:57 +0530

    Update api.js
    change base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://mern-notes-app-7m51.onrender.com'/api,
+  baseURL: 'https://notes-app-2-b.onrender.com,
   withCredentials: true, 
 });
 
