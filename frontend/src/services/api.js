a039f8e251184fa1f4e1b5d753b304c2d9bfd4a5
Mohd Fazil <fazilansari038@gmail.com>
2026-04-17 10:37:35 +0530

    Update api.js
    add "  ' " this in base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://notes-app-2-b.onrender.com,
+  baseURL: 'https://notes-app-2-b.onrender.com',
   withCredentials: true, 
 });
 
