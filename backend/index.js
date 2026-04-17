e2cafb7df2848388a869174d701122993fa40d7b
Mohd Fazil <fazilansari038@gmail.com>
2026-04-17 10:16:59 +0530

    Update index.js
    chage origin for rela live url of frontend

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // 2. CORS update for Cookies (Frontend URL yahan aayega)
 app.use(cors({
-  origin: 'https://fazil-note-app3.netlify.app/', // Exact frontend URL
+  origin: 'https://notes-app-2-yb3s.onrender.com', // Exact frontend URL
   credentials: true,               // Cookies ke liye zaroori
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
